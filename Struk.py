eef79fd85f93ee09bc00e6a1273a1fda895a11f8
Mayooo_ <arnoldomario97@gmail.com>
2022-10-29 14:07:00 +0900

    menambahkan file struk kasir.py

---
+++ Struk.py
@@ -0,0 +1,59 @@
+# MEMBUAT STRUK BELANJA
+import datetime
+
+waktu = datetime.datetime.now().strftime("%x")
+tanggal = datetime.datetime.now().strftime("%X")
+
+namakasir = input("Masukan nama kasir : ")
+# Tampilan awal
+print(f"""
+PT INDOMARCO PRISMATAMA      |===============|
+GEDUNG MENARA INDOMARET      |==|OYOOMARET|==|
+BOULEVARD PANTAI INDAH KAPUK |===============|
+JAKARTA UTARA
+NPWP.01.337.994.6-092.000
+
+             YOS SUDARSO
+        JL. YOS SUDARSO NO. 12
+          KOTA SORONG, 98413
+----------------------------------------
+    {tanggal}-{waktu} 935349/{namakasir}/01
+----------------------------------------
+""")
+
+tambahbarang = input("Apakah ingin mengscan barang ? \n").lower()
+while "y" in tambahbarang == "y" :
+
+    namabarang = input("Masukan nama barang :  ")
+
+    hargabarang = int(input("Masukan harga barang :  "))
+
+    jumlahbarang = int(input("Masukan jumlah barang :  "))
+
+    # hargatotal += (hargabarang * jumlahbarang)
+    # jumlahbelanjaan += jumlahbarang
+    
+    tambahbarang = input("Apakah ingin mengscan barang lagi (y/n) ? ").lower()
+    
+# Tampilkan barang
+
+# tunai = int(input("MASUKAN NOMINAL TUNAI : "))
+# kembalian = tunai - hargatotal
+
+print(f"""
+----------------------------------------
+{namabarang} {jumlahbarang} Pcs Rp.{hargabarang}     Rp.{hargatotal}
+----------------------------------------
+TOTAL   : {hargatotal}
+TUNAI   : {tunai}
+KENBALI : {kembalian}
+""")
+
+
+print("""
+PPN       : DPP= 17,838  PPN= 1,962
+ID POINKU : xxxxxxxx142/Maxxxxxxxx Axxxxxx
+CEK PEROLEHAN POIN/STAMP/LUCKY DI APPS POINKU
+    LAYANAN KOSUMEN SMS 0811 1500 280
+ CALL 1500 280 - KONTAK@INDOMARET.CO.ID
+""")
