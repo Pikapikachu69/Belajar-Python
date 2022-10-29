c0e9604843e892cbe6c460f03656f838d7fb84b9
Mayooo_ <arnoldomario97@gmail.com>
2022-10-29 15:00:37 +0900

    memperbaiki file struk.py

--- Struk.py
+++ Struk.py
@@ -1,14 +1,15 @@
 # MEMBUAT STRUK BELANJA
 import datetime
 
-waktu = datetime.datetime.now().strftime("%x")
-tanggal = datetime.datetime.now().strftime("%X")
+waktu = datetime.datetime.now().strftime("%X")
+tanggal = datetime.datetime.now().strftime("%x")
+
 
 namakasir = input("Masukan nama kasir : ")
 # Tampilan awal
 print(f"""
 PT INDOMARCO PRISMATAMA      |===============|
-GEDUNG MENARA INDOMARET      |==|OYOOMARET|==|
+GEDUNG MENARA INDOMARET      |==|INDOMARET|==|
 BOULEVARD PANTAI INDAH KAPUK |===============|
 JAKARTA UTARA
 NPWP.01.337.994.6-092.000
@@ -20,40 +21,52 @@ NPWP.01.337.994.6-092.000
     {tanggal}-{waktu} 935349/{namakasir}/01
 ----------------------------------------
 """)
-
-tambahbarang = input("Apakah ingin mengscan barang ? \n").lower()
+# 
+hargatotal = 0
+jumlahbelanjaan = 0
+tambahbarang = input("Apakah ingin mengscan barang (y/n) ? \n").lower()
 while "y" in tambahbarang == "y" :
 
-    namabarang = input("Masukan nama barang :  ")
+    namabarang = input("Masukan nama barang   :  ")
 
-    hargabarang = int(input("Masukan harga barang :  "))
+    hargabarang = int(input("Masukan harga barang  :  Rp"))
 
     jumlahbarang = int(input("Masukan jumlah barang :  "))
 
-    # hargatotal += (hargabarang * jumlahbarang)
-    # jumlahbelanjaan += jumlahbarang
+    total = hargabarang * jumlahbarang
+    
+    print(f"""
+    ----------------------------------------
+    {namabarang} {jumlahbarang} Pcs Rp.{hargabarang}    Total : Rp.{total:,}
+    ----------------------------------------
+    """)
+    hargatotal += (hargabarang * jumlahbarang)
+    jumlahbelanjaan += jumlahbarang
     
     tambahbarang = input("Apakah ingin mengscan barang lagi (y/n) ? ").lower()
     
 # Tampilkan barang
 
-# tunai = int(input("MASUKAN NOMINAL TUNAI : "))
-# kembalian = tunai - hargatotal
+print(f"Harga Total : Rp.{hargatotal}")
+tunai = int(input("MASUKAN NOMINAL TUNAI : Rp."))
+kembalian = tunai - hargatotal
 
 print(f"""
 ----------------------------------------
-{namabarang} {jumlahbarang} Pcs Rp.{hargabarang}     Rp.{hargatotal}
+{jumlahbelanjaan} Pcs Rp.{hargatotal:,}
 ----------------------------------------
-TOTAL   : {hargatotal}
-TUNAI   : {tunai}
-KENBALI : {kembalian}
 """)
 
+print(f"""
+        TOTAL   : {hargatotal:,}
+        TUNAI   : {tunai:,}
+        KENBALI : {kembalian:,}
 
-print("""
 PPN       : DPP= 17,838  PPN= 1,962
 ID POINKU : xxxxxxxx142/Maxxxxxxxx Axxxxxx
 CEK PEROLEHAN POIN/STAMP/LUCKY DI APPS POINKU
-    LAYANAN KOSUMEN SMS 0811 1500 280
- CALL 1500 280 - KONTAK@INDOMARET.CO.ID
+     LAYANAN KOSUMEN SMS 0811 1500 280
+  CALL 1500 280 - KONTAK@INDOMARET.CO.ID
+
+  TERIMAKASIH TELAH BERBELANJA DI INDOMARET
 """)
